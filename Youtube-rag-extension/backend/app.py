39d9caa8241532cf0cb89a6ce9d737fb32463f0b
Saharsh-Bhave <saharshsbhave@arizona.edu>
2025-10-23 23:30:21 -0700

    API for chrome extension using FAST API

---
+++ Youtube-rag-extension/backend/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag_pipeline import get_answer
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+class Query(BaseModel):
+    question: str
+    video_id: str
+
+@app.post("/ask")
+def ask_youtube(data: Query):
+    answer = get_answer(data.video_id, data.question)
+    return{"answer": answer}
